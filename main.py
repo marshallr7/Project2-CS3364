99be8f45977449fad913ae321a0340e71b4c09a3
Marshall <marspatt@ttu.edu>
2023-03-26 17:35:58 -0500

    Adds read_courses

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
-def print_hi(name):
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+def read_courses(file_name):
+    courses = {}
+    prerequisites = []
+
+    with open(file_name, 'r') as file:
+        lines = file.readlines()
+        for line in lines:
+            course, prereq = line.strip().split(' - ')
+            if prereq != 'N/A':
+                prerequisites.append((course, prereq))
+            if course not in courses:
+                courses[course] = len(courses)
+
+    return courses, prerequisites
 
 
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    print(read_courses("courses.txt"))
 
